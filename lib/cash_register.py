161dad07959a9f6b391b1785b981fbba63b68544
Wesley Peters <wesleypeters@gmail.com>
2023-07-05 21:37:49 -0600

    create class and methods

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+        self.last_transaction = {}
+
+    def add_item(self, title, price, quantity=1):
+        self.total += price * quantity
+        self.items.extend([title]*quantity)
+        self.last_transaction = {'title': title, 'price': price, 'quantity': quantity}
+
+    def apply_discount(self):
+        if self.discount:
+            self.total = self.total * (1 - (self.discount / 100))
+            print(f"After the discount, the total comes to ${int(self.total)}.")
+        else:
+            print("There is no discount to apply.")
+
+    def void_last_transaction(self):
+        self.total -= self.last_transaction['price'] * self.last_transaction['quantity']
+        for _ in range(self.last_transaction['quantity']):
+            self.items.remove(self.last_transaction['title'])
+
